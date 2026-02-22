febf0acbb4f7a21a4dd5247e111da97059eb982f
Gagan Chandra <gaganchandra02@gmail.com>
2026-02-22 09:29:03 +0530

    Setup base App component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+// Pages
+import Home from './pages/Home';
+import Predict from './pages/Predict';
+import History from './pages/History';
+import About from './pages/About';
+import Auth from './pages/Auth';
+import Results from './pages/Results';
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/predict" element={<Predict />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/auth" element={<Auth />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
